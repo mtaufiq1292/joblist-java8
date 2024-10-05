823a6af07376bd845f330d61346436bc9bdd534c
Muhammad Taufiq bin Ismail <mtaufiq1292@gmail.com>
2024-10-05 09:34:56 +0800

    update

--- src/main/java/com/joblist/JoblistApplication.java
+++ src/main/java/com/joblist/JoblistApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JoblistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JoblistApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JoblistApplication.class, args);
+    }
 
 }
